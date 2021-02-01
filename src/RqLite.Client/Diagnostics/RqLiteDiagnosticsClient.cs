2b2d2151c196820187a7d2c0147c3628165199aa
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-02-01 11:46:42 +0100

    diagnostics client and model serializer

---
+++ src/RqLite.Client/Diagnostics/RqLiteDiagnosticsClient.cs
@@ -0,0 +1,20 @@
+﻿using RqLite.Client.Diagnostics.Model;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RqLite.Client.Diagnostics
+{
+    public class RqLiteDiagnosticsClient
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<RqLiteDiagnosticsConfiguration> GetDiagnostics(string uri)
+        {
+            var response = await client.GetAsync(uri + "/status");
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return  RqLiteDiagnosticsSerializer.FromJson(await response.Content.ReadAsStringAsync());
+            throw new Exception(response.StatusCode.ToString());
+        }
+    }
+}
---
+++ src/RqLite.Client/Diagnostics/RqLiteDiagnosticsSerializer.cs
@@ -0,0 +1,11 @@
+﻿using RqLite.Client.Diagnostics.Model;
+using System.Text.Json;
+
+namespace RqLite.Client.Diagnostics
+{
+    public static class RqLiteDiagnosticsSerializer
+    {
+        public static RqLiteDiagnosticsConfiguration FromJson(string json) => JsonSerializer.Deserialize<RqLiteDiagnosticsConfiguration>(json);
+        public static string ToJson(this RqLiteDiagnosticsConfiguration self) => JsonSerializer.Serialize(self);
+    }
+}
